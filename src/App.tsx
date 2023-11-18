a0a3d2c74932cc1b7a20189a92b5d0b0dbff8f03
Abdul Gamal <abdul.nanjira@gmail.com>
2023-11-19 00:00:31 +0300

    installed shadcn/ui

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,11 @@
 import "./App.css";
+import { Button } from "./components/ui/button";
 
 function App() {
   return (
     <>
       <h1 className="text-red-400">Snap app</h1>
+      <Button>Shadcn Btn</Button>
     </>
   );
 }
